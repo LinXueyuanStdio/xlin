a47759ff649a20d0efc5c390cfad97e513598746
兮尘 <23211526+LinXueyuanStdio@users.noreply.github.com>
2025-03-23 16:15:47 +0800

    ✨ [add xlin/__init__.py] Include timing module in imports

--- xlin/__init__.py
+++ xlin/__init__.py
@@ -4,6 +4,7 @@ from .metric import *
 from .multiprocess_mapping import *
 from .read_as_dataframe import *
 from .statistic import *
+from .timing import *
 from .util import *
 from .xls2xlsx import *
 from .yaml import *
